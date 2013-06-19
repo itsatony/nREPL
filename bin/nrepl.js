4422492a686d366a2b4644d878d166208113a5e8
Toni Wagner <i@itsatony.com>
2013-06-19 14:42:53 +0200

    working version with node 0.10.x

--- bin/nrepl.js
+++ bin/nrepl.js
@@ -6,6 +6,11 @@ if (typeof process.argv[2] === 'undefined') {
 	console.log('nREPL ---[[[ Usage: nrepl <port|host port> ]]]---');
 	console.log('nREPL ---[[[ Usage: nrepl serve <port> ]]]--- will start a repl server. this makes sense for testing only i guess...');
 	process.exit();
+} else if (process.argv[2] === 'serve' && typeof process.argv[3] !== 'undefined') {
+	var os = require('os');
+	var serverId = os.hostname();
+	nrepl.nReplServer(serverId, parseInt(process.argv[3]));
+} else {
+	nrepl.nReplClient(process.argv[2], process.argv[3]);
 }
 
-nrepl.nReplClient(process.argv[2], process.argv[3]);
